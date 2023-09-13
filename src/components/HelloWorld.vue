465711033e5c1d5944e249460b9a20f323db0054
vietnt084 <viet.nguyen@amela.vn>
2023-09-13 09:12:25 +0700

    add calculate money

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,31 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="hello" style="text-align: -webkit-center;">
+    <h2>The Money follow year for finance happiness</h2>
+    <div style="text-align:left; width:30%">
+      <div style="display: block;">
+          Init money: <input v-model="initMoney" />
+      </div>
+      <div style="display: block;">
+          AccelerationInvest: <input v-model="accelerationInvest"/>
+      </div>
+      <div style="display: block;">
+          accelerationInvestGrowth: <input v-model="accelerationInvestGrowth" />
+      </div>
+      <div style="display: block;">
+          expectProfit: <input v-model="expectProfit" />
+      </div>
+      <div style="display: block; margin-bottom:30px;">
+          expectMoney: <input v-model="expectMoney" />
+      </div>
+
+      <button @click="calculate()">
+        Caculate Money
+      </button>
+
+      <div v-for="(money, index) in moneyOfYear" :key="`money-${index}`">
+        Money after {{index + 1}} year: {{money}}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,6 +34,49 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      moneyOfYear: [],
+      initMoney: 300,
+      accelerationInvest: 120,
+      expectProfit: 0.2,
+      expectMoney: 6000,
+      accelerationInvestGrowth: 0.1
+    }
+  },
+
+  mounted() {
+  },
+
+  methods: {
+    calculate(){
+      this.moneyOfYear = []
+      this.caculateOfMoney(this.initMoney, this.accelerationInvest, this.accelerationInvestGrowth, this.expectProfit, this.expectMoney)
+    },
+    caculateOfMoney(initMoney, accelerationInvest, accelerationInvestGrowth, expectProfit, expectMoney, numberOfYear = 1) {
+      //initMoney: Số tiền đầu tư ban đầu
+      //accelerationInvest: Gia tốc đầu tư hằng năm
+      //expectProfit: Lợi nhuận kỳ vọng 1 năm
+      //expectMoney: Số tiền mong muốn tự do tài chính
+      //accelerationInvestGrowth: Tỷ lệ tăng gia tốc hằng năm
+
+      initMoney = parseFloat(initMoney)
+      accelerationInvestGrowth = parseFloat(accelerationInvestGrowth)
+      expectProfit = parseFloat(expectProfit)
+      expectMoney = parseFloat(expectMoney)
+      accelerationInvest = parseFloat(accelerationInvest)
+
+      const totalInvest = parseFloat(initMoney + accelerationInvest + (accelerationInvest * accelerationInvestGrowth))
+      const R = totalInvest + (totalInvest * expectProfit)
+      this.moneyOfYear.push(R)
+      if(R > expectMoney) {
+        return 'fine'
+      } else {
+        numberOfYear = numberOfYear + 1
+      }
+      return this.caculateOfMoney(R, accelerationInvest, accelerationInvestGrowth, expectProfit, expectMoney, numberOfYear)
+    }
   }
 }
 </script>
